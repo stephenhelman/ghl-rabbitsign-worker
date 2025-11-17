e6b1cca324579ca785f26beef99857c4b90dede7
Stephen Helman <stephenhelman18@gmail.com>
2025-11-17 10:33:01 -0700

    minor fixes

--- src/prefill/mapping.js
+++ src/prefill/mapping.js
@@ -2,7 +2,7 @@ import { splitTitle } from "../util/util";
 import { buildSenderFieldValuesForType, buildRoles } from "./senderFields";
 
 export function buildRabbitPrefillPayload(body, date, title) {
-  const [type] = splitTitle(title);
+  const { type } = splitTitle(title);
   const summary = `Seller: ${body.customData.sellerName || "Unknown"}`;
   const senderFieldValues = buildSenderFieldValuesForType(type, body, date);
   const roles = buildRoles(body);
