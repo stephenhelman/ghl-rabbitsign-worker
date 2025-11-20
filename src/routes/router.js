6eb1701c6821abb51d89abee767e448f459018eb
Stephen Helman <stephenhelman18@gmail.com>
2025-11-19 17:22:06 -0700

    Removing the helper branch

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,5 @@
 import { handlePrefillRequest } from "../prefill/handler.js";
 import { handleRabbitWebhook } from "../webhook/handler.js";
-import { handleTestUpload } from "../test/handler.js";
 
 export async function handleRequest(request, env, ctx) {
   const url = new URL(request.url);
@@ -24,10 +23,6 @@ export async function handleRequest(request, env, ctx) {
   // prefill endpoint TODO
   if (pathname.startsWith("/prefill/") && method === "POST") {
     return handlePrefillRequest(request, env);
-  }
-
-  if (pathname === "/test-worker" && method === "GET") {
-    return handleTestUpload(request, env, ctx);
   }
 
   return new Response("Not found", { status: 404 });
